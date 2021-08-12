e1aa0ea49301919e47e8feede266b9734fbd09ab
Jemila Mickle <88526006+jemilamickle@users.noreply.github.com>
2021-08-12 17:40:52 +0530

    Add files via upload

---
+++ Day-04-ilif-nest/year.py
@@ -0,0 +1,10 @@
+currentage=int(input("enter a currentage"))
+futureage=90
+year=futureage-currentage
+print(f"the year is {year}")
+month=futureage*12 - currentage*12
+print(f"the month is {month}")
+week=futureage*52 - currentage*52
+print(f"the week is {week}")
+day=futureage*365 - currentage*365
+print(f"the day is {day}")
