a2dca7ce2934b8bab481f7966c0a270b49261b4b
Jemila Mickle <88526006+jemilamickle@users.noreply.github.com>
2021-08-12 14:25:04 +0530

    Add files via upload

---
+++ Day-03-conditional/conditional.py
@@ -0,0 +1,9 @@
+x=int(input("enter an age"))
+if(5)<x<(10):
+    print(f"children")
+elif(10)<x<(20):
+    print(f"teen")
+elif(20)<x<(30):
+    print(f"middle age")
+else:
+    print(f"yor are mature")
